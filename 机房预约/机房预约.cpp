02780e44e475a94e34fbd41076c1cfc71c2f1fc6
threewater32 <85158236+threewater32@users.noreply.github.com>
2023-03-14 17:35:58 +0800

    1

---
+++ 机房预约/机房预约.cpp
@@ -0,0 +1,40 @@
+﻿// 机房预约.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+
+#include"机房预约.h"
+#include <iostream>
+
+using namespace std;
+void show_Menu() {
+
+
+	cout << "======================  欢迎来到传智播客机房预约系统  =====================" << endl;
+	cout << endl << "请输入您的身份" << endl;
+	cout << "\t\t -------------------------------\n";
+	cout << "\t\t|                               |\n";
+	cout << "\t\t|          1.学生代表           |\n";
+	cout << "\t\t|                               |\n";
+	cout << "\t\t|          2.老    师           |\n";
+	cout << "\t\t|                               |\n";
+	cout << "\t\t|          3.管 理 员           |\n";
+	cout << "\t\t|                               |\n";
+	cout << "\t\t|          0.退    出           |\n";
+	cout << "\t\t|                               |\n";
+	cout << "\t\t -------------------------------\n";
+	cout << "输入您的选择: ";
+}
+int main()
+{
+
+
+	show_Menu();
+
+
+
+
+
+
+
+
+
+    std::cout << "Hello World!\n";
+}
