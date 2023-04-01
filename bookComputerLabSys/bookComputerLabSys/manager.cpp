541cafb54f19ffdc34461b9000c171f7cbf234e6
threewater32 <85158236+threewater32@users.noreply.github.com>
2023-04-01 20:15:08 +0800

    1

--- bookComputerLabSys/bookComputerLabSys/manager.cpp
+++ bookComputerLabSys/bookComputerLabSys/manager.cpp
@@ -1,23 +1,92 @@
 #include "manager.h"
+#include "bookComputerLabSys.h"
 
 Manager::Manager()
 {
-	
+
 }
 
 Manager::Manager(int id, string name, string pwd)
 {
 	this->m_Name = name;
 	this->m_Pwd = pwd;
+	
+	this->initVector();
 }
 
 void Manager::operMenu()
 {
-
+	
+		cout << "欢迎管理员：" << this->m_Name << "登录！" << endl;
+		cout << "\t\t ---------------------------------\n";
+		cout << "\t\t|                                |\n";
+		cout << "\t\t|          1.添加账号            |\n";
+		cout << "\t\t|                                |\n";
+		cout << "\t\t|          2.查看账号            |\n";
+		cout << "\t\t|                                |\n";
+		cout << "\t\t|          3.查看机房            |\n";
+		cout << "\t\t|                                |\n";
+		cout << "\t\t|          4.清空预约            |\n";
+		cout << "\t\t|                                |\n";
+		cout << "\t\t|          0.注销登录            |\n";
+		cout << "\t\t|                                |\n";
+		cout << "\t\t ---------------------------------\n";
+		cout << "请选择您的操作： " << endl;
+	
+	
 }
 
 void Manager::addPerson()
 {
+	cout << "请输入添加账号的类型" << endl;
+	cout << "1、添加学生" << endl;
+	cout << "2、添加老师" << endl;
+
+
+
+	string fileName;
+	string tip;
+	ofstream ofs;
+
+	int select = 0;
+	cin >> select;
+
+	if (select == STUDENT)
+	{
+		fileName = STUDENT_FILE;
+		tip = "请输入学号： ";
+	}
+	else if (select == TEACHER)
+	{
+		fileName = TEACHER_FILE;
+		tip = "请输入职工编号：";
+	}
+	else
+	{
+		cout << "输入错误" << endl;
+		return;
+	}
+
+	ofs.open(fileName, ios::out | ios::app);
+	int id;
+	string name;
+	string pwd;
+	cout << tip << endl;
+	cin >> id;
+
+	cout << "请输入姓名： " << endl;
+	cin >> name;
+
+	cout << "请输入密码： " << endl;
+	cin >> pwd;
+
+	ofs << id << " " << name << " " << pwd << " " << endl;
+	cout << "添加成功" << endl;
+
+	system("pause");
+	system("cls");
+
+	ofs.close();
 
 }
 
@@ -35,3 +104,51 @@ void Manager::cleanFile()
 {
 
 }
+
+void Manager::initVector()
+{
+	vStu.clear();
+	vTea.clear();
+
+	//读取学生文件中信息
+	ifstream ifs;
+	ifs.open(STUDENT_FILE, ios::in);
+	if (!ifs.is_open())
+	{
+		cout << "文件读取失败" << endl;
+		return;
+	}
+
+
+	Student s;
+	while (ifs>>s.m_Id&&ifs>>s.m_Name&&ifs>>s.m_Pwd)
+	{
+		vStu.push_back(s);
+
+	}
+	cout << "当前学生数量为： " << vStu.size() << endl;
+	ifs.close();
+	
+	//读取老师文件中信息
+	
+	ifs.open(TEACHER_FILE, ios::in);
+	if (!ifs.is_open())
+	{
+		cout << "文件读取失败" << endl;
+		return;
+	}
+
+
+	Teacher t;
+	while (ifs >> t.m_EmId && ifs >> t.m_Name && ifs >> t.m_Pwd)
+	{
+		vTea.push_back(t);
+
+	}
+	cout << "当前老师数量为： " << vStu.size() << endl;
+	ifs.close();
+
+
+
+}
+
